bcf634fce3cf9445002f73410b0027d77dcc037f
Max01112 <maxim.reancev@mail.ru>
2026-09-05 11:26:44 +0300

    All Tusk Completed

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,100 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!!");
+
+
+        System.out.println(" ");
+        System.out.println("Задание 1");
+
+        int clientOS = 0;
+        switch (clientOS) {
+            case 0:
+                System.out.println("У пользователя iOS");
+                break;
+            case 1:
+                System.out.println("У пользователя Android");
+                break;
+            default:
+                System.out.println("В списке нет такой ОС");
+        }
+
+        System.out.println(" ");
+        System.out.println("Задание 2");
+        int clientDeviceYear = 2015;
+        clientOS = 0;
+        switch (clientOS) {
+            case 0:
+                if (clientDeviceYear < 2015) {
+                    System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+                } else {
+                    System.out.println("Установите версию приложения для iOS по ссылке");
+                }
+                break;
+            case 1:
+                if (clientDeviceYear < 2015) {
+                    System.out.println("Установите облегченную версию приложения для Android по ссылке");
+                } else {
+                    System.out.println("Установите версию приложения для Android по ссылке");
+                }
+                break;
+            default:
+                System.out.println("В списке нет такой ОС");
+        }
+
+        System.out.println(" ");
+        System.out.println("Задание 3");
+        int year = 1900;
+        if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
+
+        System.out.println(" ");
+        System.out.println("Задание 4");
+        int deliveryDistance = 95;
+        if (deliveryDistance < 20) {
+            System.out.println("Потребуется дней: " + 1);
+        } else if (deliveryDistance >= 20 && deliveryDistance < 60) {
+            System.out.println("Потребуется дней: " + 2);
+        } else if (deliveryDistance >= 60 && deliveryDistance < 100) {
+            System.out.println("Потребуется дней: " + 3);
+        } else {
+            System.out.println("Слишком большой маршрут");
+        }
+
+        System.out.println(" ");
+        System.out.println("Задание 5");
+        int monthNumber = 12;
+        switch (monthNumber) {
+            case 12:
+            case 1:
+            case 2: {
+                System.out.println("Зима");
+
+            }
+            break;
+            case 3:
+            case 4:
+            case 5: {
+                System.out.println("Весна");
+
+            }
+            break;
+            case 6:
+            case 7:
+            case 8: {
+                System.out.println("Лето");
+
+            }
+            break;
+            case 9:
+            case 10:
+            case 11: {
+                System.out.println("Осень");
+            }
+            default:
+                System.out.println("Такого месяца нет!");
         }
     }
+}
