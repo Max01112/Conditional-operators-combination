9dd5701c83694008bec2b45013e81ae4a54d3e1d
Max01112 <maxim.reancev@mail.ru>
2026-09-06 10:08:19 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -9,7 +9,7 @@ public class Main {
 
         int clientOS = 1;
         if (clientOS == 0) {
-            System.out.println("Установите версию приложения для iOS по ссылке");
+            System.out.println("Установите версию приложения для iOS по ссылке!");
         } else if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
